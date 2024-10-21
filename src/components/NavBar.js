d51958fc64b4f78814bbfd0f79c7062fd6c78c55
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-10-21 08:48:53 +0000

    Add styling to App.js and to the navbar separately

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from "../assets/logo.png";
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
-    return <Navbar expand="md" fixed="top">
+    return <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
             <Navbar.Brand>
                 <img src={logo} alt='logo' height='45'/>
@@ -11,9 +12,9 @@ const NavBar = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <Nav.Link><i className='fas fa-home'></i> Home</Nav.Link>
-                    <Nav.Link><i className='fas fa-sign-in-alt'></i> Sign in</Nav.Link>
-                    <Nav.Link><i className='fas fa-user-plus'></i> Sign up</Nav.Link>
+                    <Nav.Link><i className='fas fa-home'></i>Home</Nav.Link>
+                    <Nav.Link><i className='fas fa-sign-in-alt'></i>Sign in</Nav.Link>
+                    <Nav.Link><i className='fas fa-user-plus'></i>Sign up</Nav.Link>
 
                 </Nav>
 
