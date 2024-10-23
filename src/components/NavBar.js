0110637b5eca45db09bdc79acf5ea789f4e32cc6
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-10-23 14:09:22 +0000

    Add Avatar.js file and avatar.module.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,50 @@ import logo from "../assets/logo.png";
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from 'react-router-dom'
 import { useCurrentUser } from '../contexts/CurrentUserContext';
+import Avatar from './Avatar';
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
-    const loggedInIcons = <>{currentUser?.username}</>
+    const addPostIcon = (
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/posts/create"
+        >
+            <i className='fas fa-plus-square'></i>Add post
+        </NavLink>
+    )
+    const loggedInIcons =
+        <>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/feed"
+            >
+                <i className='fas fa-stream'></i>Feed
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/liked"
+            >
+                <i className='fas fa-heart'></i>Liked
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                to="/"
+                onClick={() => {}}
+            >
+                <i className='fas fa-sign-out-alt'></i>Sign out
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                to={`/profiles/${currentUser?.profile_id}`}
+            >
+                <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+            </NavLink>
+
+        </>
     const loggedOutIcons = (
         <> <NavLink
             className={styles.NavLink}
@@ -25,29 +65,33 @@ const NavBar = () => {
         </>
     );
 
-    return <Navbar className={styles.NavBar} expand="md" fixed="top">
-        <Container>
-            <NavLink to="/">
-                <Navbar.Brand>
-                    <img src={logo} alt='logo' height='45' />
-                </Navbar.Brand></NavLink>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto text-left">
-                    <NavLink 
-                    exact className={styles.NavLink} 
-                    activeClassName={styles.Active} 
-                    to="/"
-                >
-                    <i className='fas fa-home'></i>Home
+    return (
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
+            <Container>
+                <NavLink to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt='logo' height='45' />
+                    </Navbar.Brand>
                 </NavLink>
-                {currentUser ? loggedInIcons : loggedOutIcons}
+                {currentUser && addPostIcon}
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-left">
+                        <NavLink
+                            exact className={styles.NavLink}
+                            activeClassName={styles.Active}
+                            to="/"
+                        >
+                            <i className='fas fa-home'></i>Home
+                        </NavLink>
+                        {currentUser ? loggedInIcons : loggedOutIcons}
 
-                </Nav>
+                    </Nav>
 
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
 };
 
 export default NavBar;
