6796ca9a8adf585529f651d4b87b5c3ad73b410d
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-10-23 13:05:13 +0000

    Add interceptors to refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://codestar100-1ca13721cbe6.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
