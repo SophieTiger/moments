0de29ce2bbecd72b030248ab05d92d2eafe9aa8f
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-10-29 10:42:59 +0000

    Add popperConfig to dropdown menu

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -15,23 +15,26 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
     />
 ));
 
-export const MoreDropdown = ({handleEdit, handleDelete}) => {
+export const MoreDropdown = ({ handleEdit, handleDelete }) => {
     return (
         <Dropdown className="ml-auto" drop="left">
             <Dropdown.Toggle as={ThreeDots} />
 
 
-            <Dropdown.Menu className="text-center">
-                <Dropdown.Item 
-                className={styles.DropdownItem}
-                onClick={handleEdit}
-                aria-label='edit'
+            <Dropdown.Menu
+                className="text-center"
+                popperConfig={{ strategy: "fixed" }}
+            >
+                <Dropdown.Item
+                    className={styles.DropdownItem}
+                    onClick={handleEdit}
+                    aria-label='edit'
                 >
-                    <i className="fas fa-edit"/>
+                    <i className="fas fa-edit" />
                 </Dropdown.Item>
                 <Dropdown.Item className={styles.DropdownItem}
-                onClick={handleDelete}
-                aria-label="delete" 
+                    onClick={handleDelete}
+                    aria-label="delete"
                 >
                     <i className="fas fa-trash-alt" />
                 </Dropdown.Item>
