076c98320cb5332939c40cabf73ac45db1db1a36
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-10-29 09:38:48 +0000

    Add infinite scroll functionality to PostsPage

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,6 +14,8 @@ import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults"
 
 import NoResults from "../../assets/no-results.png"
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 
 function PostsPage({ message, filter = "" }) {
@@ -62,12 +64,23 @@ function PostsPage({ message, filter = "" }) {
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              posts.results.map(post => (
-                <Post key={post.id} {...post} setPosts={setPosts} />
-              ))
-            ) : <Container className={appStyles.Content}>
-              <Asset src={NoResults} message={message} />
-            </Container>}
+              <InfiniteScroll
+                children={
+                  posts.results.map((post) => (
+                    <Post key={post.id} {...post} setPosts={setPosts} />
+                  ))}
+                  dataLength={posts.results.length}
+                  loader={<Asset spinner />}
+                  hasMore={!!posts.next}
+                  next={() => fetchMoreData(posts, setPosts)}
+              />
+
+
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
           </>
         ) : (
           <Container className={appStyles.Content}>
