b7661ecb362e3db2896ea22b1b295c482ec6f206
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-05 13:47:11 +0000

    Add timestamp for tokens in localstorage and handle it via utils.js

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -2,7 +2,7 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "../NavBar";
 import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
-import { scryRenderedComponentsWithType } from "react-dom/test-utils";
+
 
 test("renders NavBar", () => {
     render(
